c62a5bff9580c55b607a7732557956c391c39f41
MatheusPetrachin <mrp.petrachin@gmail.com>
2020-05-15 23:35:03 -0300

    Entidades Iniciais

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    class Usuario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICollection<Pedido> Pedidos { get; set; }
+    }
+}
