251276d0de26521a9f46ebe6718359d8eecb4d2b
MatheusPetrachin <mrp.petrachin@gmail.com>
2020-05-29 21:38:31 -0300

    Layout Login

---
+++ QuickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from "@angular/core";
+import { Usuario } from "../../model/usuario";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
+})
+export class LoginComponent {
+
+  public usuario;
+  public usuarioAutenticado: boolean;
+
+  constructor() {
+    this.usuario = new Usuario();
+  }
+
+  entrar() {
+    if (this.usuario.email == "mpetrachin@hdlao.com" && this.usuario.password == "testex") {
+      this.usuarioAutenticado = true;
+    }
+  }
+
+}
