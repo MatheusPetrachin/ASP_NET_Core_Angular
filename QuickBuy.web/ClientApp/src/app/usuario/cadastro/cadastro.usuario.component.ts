5ed997c05bafc6b5018c421907c1e5b91d2523dc
MatheusPetrachin <mrp.petrachin@gmail.com>
2020-07-06 10:42:08 -0300

    DEV

--- QuickBuy.web/ClientApp/src/app/usuario/cadastro/cadastro.usuario.component.ts
+++ QuickBuy.web/ClientApp/src/app/usuario/cadastro/cadastro.usuario.component.ts
@@ -1,1 +1,10 @@
-﻿
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "cadastro-usuario",
+  template: "./cadastro.usuario.component.html",
+  styleUrls: ["./cadastro.usuario.component.css"]
+})
+export class CadastroUsuarioComponent {
+
+}
