b354ec16123bd243d85f2b974696bdbd71fbfca0
MatheusPetrachin <mrp.petrachin@gmail.com>
2020-07-06 14:14:56 -0300

    Injetando serviço no CadastroComponent

--- QuickBuy.web/ClientApp/src/app/usuario/cadastro/cadastro.usuario.component.ts
+++ QuickBuy.web/ClientApp/src/app/usuario/cadastro/cadastro.usuario.component.ts
@@ -1,10 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Usuario } from "../../model/usuario";
+import { UsuarioServico } from "../../servico/usuario/usuario.servico";
 
 @Component({
   selector: "cadastro-usuario",
   template: "./cadastro.usuario.component.html",
   styleUrls: ["./cadastro.usuario.component.css"]
 })
-export class CadastroUsuarioComponent {
+export class CadastroUsuarioComponent implements OnInit {
+
+  public usuario: Usuario;
+
+  constructor(
+    private usuarioServico: UsuarioServico
+  ) { };
+
+  ngOnInit(): void {
+    this.usuario = new Usuario;
+  }
+
 
 }
